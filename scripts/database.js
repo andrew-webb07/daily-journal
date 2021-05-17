aa7168da31c45860548de92ed2dc7031e9a1b180
Andrew Webb <webba@ihopu.org>
2021-05-17 10:34:04 -0500

    displayed entries on DOM

--- scripts/database.js
+++ scripts/database.js
@@ -7,22 +7,50 @@
  */
 
 const database = {
-    entries: []
+   entries: []
 }
 
 /*
     You export a function that provides a version of the
     raw data in the format that you want
 */
-export const getJournalEntries = () => {
-    const copyOfData = [...database.entries]
-    return copyOfData
+
+const container = document.querySelector("#entries")
+
+export const fetchEntries = () => {
+    return fetch("http://localhost:8088/entries") // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(
+            (data) => {
+            database.entries = data// What should happen when we finally have the array?
+        }
+    )
 }
 
+
 export const getEntries = () => {
-    return fetch("http://localhost:8088/entries") // Fetch from the API
-        .then(entries => entries.json())  // Parse as JSON
-        .then(entries => {
-            return entries.push(database.entries)// What should happen when we finally have the array?
-        })
+    return [...database.entries]
+}
+
+export const saveJournalEntry = (entryObject) => {
+    // Use `fetch` with the POST method to add your entry to your API
+fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newJournalEntry)
+})
+    .then(
+        (response) => {
+            //  Get all journal entries
+            response.json()
+        }
+    )
+    .then(
+        () => {
+            //  Broadcast the state change event
+            container.dispatchEvent(new CustomEvent("stateChanged"))
+        }
+    )
 }
